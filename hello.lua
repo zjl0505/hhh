ff3adfc03cf9560bb9400e2241920b52ec908166
zjl0505 <jialin@JialindeMacBook-Pro.local>
2016-04-28 16:26:11 +0800

    kk

---
+++ hello.lua
@@ -0,0 +1,6 @@
+
+local num1,num2,num3 = lua_GetNumbe(10,20); -- 10 20 11 *(栈顶)
+print("Lua_num1 = ",num1); --20  -- 10
+print("Lua_num2 = ",num2); --11  -- 20
+print("Lua_num3 = ",num3);		 -- 11
+print("********")
